f722004b1daebc8a376b2ff579e219faf49853fd
Wilmer Gulcochia <karlwgs1989@gmail.com>
2026-05-03 10:28:25 -0500

    feat: declarar grid 2D y flota de vehiculos (HU-3)

--- main.cpp
+++ main.cpp
@@ -5,9 +5,16 @@
 
 using namespace std;
 
+const int FILAS = 3;
+const int COLUMNAS = 15;
+// Balance realista de vehiculos en relacion al numero de celdas del grid para evitar congestion
+const int MAX_VEHICULOS = 20;
+
 int main() {
     
     srand(time(0));
+    int grid[FILAS][COLUMNAS] = {0};
+    Vehiculo flota[MAX_VEHICULOS];
     // declaramos la variable
     Vehiculo miAuto;
     miAuto = generarVehiculoAleatorio(1);
