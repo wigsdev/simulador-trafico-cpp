0ef293af88879ee9c641ec04d7e7fd907beefded
Wilmer Gulcochia <karlwgs1989@gmail.com>
2026-05-04 11:54:02 -0500

    feat: ensamblar loop principal de simulación (HU-6)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
-#include "aleatorio.h"
+#include "modelos.h"
+#include "semaforo.h"
+#include "motor.h"
+#include "ui.h"
+#include "config.h"
 #include <iostream>
 #include <cstdlib>   // para srand() y rand()
 #include <ctime>    // para time()
-#include "config.h"
 
 using namespace std;
 
@@ -10,12 +13,29 @@ using namespace std;
 int main() {
     
     srand(time(0));
+
+    // inicializar componentes
     int grid[FILAS][COLUMNAS] = {0};
     Vehiculo flota[MAX_VEHICULOS];
-    // declaramos la variable
-    Vehiculo miAuto;
-    miAuto = generarVehiculoAleatorio(1);
-    cout << "Placa: " << miAuto.placa << endl;
-    cout << "Velocidad: " << miAuto.velocidad << endl;
-return 0;
+    Semaforo sem = {EstadoSemaforo::VERDE, 5};
+    int numVehiculos = 0;
+
+    // loop de simulacion
+    while (true) {
+        // fase de salida ui
+        // system("cls") // opcional para windows
+        dibujarPantalla(grid, sem);
+        detectarCongestion(grid);
+        
+        cout << "Presiona ENTER para el siguiente tick...";
+        cin.get(); // Pausa manual por turno
+
+        // fase de calculo
+
+        actualizarSemaforo(sem);
+        moverVehiculos(grid, flota, sem);
+        generarTrafico(grid, flota, numVehiculos);
+    }
+    
+    return 0;
 }
